49fb1ac357f206f2ab1f636ecec5108b0bda1b3e
EmmanuelObi <obiemmy123@gmail.com>
2022-10-02 22:15:04 +0100

    partial completetion of events

--- media-app/src/pages/Event/Event.js
+++ media-app/src/pages/Event/Event.js
@@ -2,27 +2,40 @@ import React from "react";
 import styles from "./Event.module.css";
 import { AppImages } from "../../assets";
 import ImageComponent from "../../components/ImageComponent/ImageComponent";
+import { useSelector } from "react-redux";
 
 const Event = () => {
+  const {
+    form: { formData },
+  } = useSelector((state) => state);
+
+  const { eventName, hostName, startDate, endDate, location } = formData ?? {};
+
+  console.log(new Date(startDate));
   return (
     <div className={styles.events}>
       <div className={styles.eventView}>
-        <h2>Birthday Bash</h2>
+        <h2>{eventName ?? ""}</h2>
         <p>
-          Hosted by <span>Elysia</span>
+          Hosted by <span>{hostName ?? ""}</span>
         </p>
         <div className={styles.info}>
           <div className={styles.date}>
             <div className={styles.icon}>Icon</div>
             <div className={styles.datetime}>
-              <span className={styles.from}>18 August 6:00PM</span>
-              <span className={styles.to}>to 19 August 1:00PM UTC +10</span>
+              <span className={styles.from}>{`${new Date(
+                startDate
+              ).toDateString()}`}</span>
+              <br />
+              <span className={styles.to}>
+                &nbsp; to {`${new Date(endDate).toDateString()}`}
+              </span>
             </div>
           </div>
           <div className={styles.location}>
             <div className={styles.icon}>Icon</div>
             <div className={styles.locate}>
-              <span className={styles.streetName}>Street name</span>
+              <span className={styles.streetName}>{location ?? ""}</span>
               <span className={styles.postal}>Suburb, State, Postcode</span>
             </div>
           </div>
